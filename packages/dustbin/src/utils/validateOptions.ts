346b5c3269aef3e1396072fa403c75f0a981f05f
老刘 <137412575@qq.com>
2023-02-24 14:21:58 +0800

    fix: change error type

--- packages/dustbin/src/utils/validateOptions.ts
+++ packages/dustbin/src/utils/validateOptions.ts
@@ -13,39 +13,39 @@ export const validateOptions = <T, CD, D>(options: QueryOptions<T, CD, D>) => {
   } = options;
 
   if (loop && retry) {
-    return new ReferenceError('[loop] [retry] cannot be used together');
+    return new Error('[loop] [retry] cannot be used together');
   }
 
   if (loop && typeof loop !== 'boolean') {
-    return new TypeError('[loop] must use boolean type');
+    return new Error('[loop] must use boolean type');
   }
 
   if (retry && typeof retry !== 'boolean') {
-    return new TypeError('[retry] must use boolean type');
+    return new Error('[retry] must use boolean type');
   }
 
   if (cacheKey && typeof cacheKey !== 'string') {
-    return new TypeError('[cacheKey] must use string type');
+    return new Error('[cacheKey] must use string type');
   }
 
   if (typeof auto !== 'boolean') {
-    return new TypeError('[auto] must use boolean type');
+    return new Error('[auto] must use boolean type');
   }
 
   if (freshTime && typeof freshTime !== 'number') {
-    return new TypeError('[freshTime] must use number type');
+    return new Error('[freshTime] must use number type');
   }
 
   if (retryInterval && typeof retryInterval !== 'number') {
-    return new TypeError('[retryInterval] must use number type');
+    return new Error('[retryInterval] must use number type');
   }
 
   if (loopInterval && typeof loopInterval !== 'number') {
-    return new TypeError('[retryInterval] must use number type');
+    return new Error('[retryInterval] must use number type');
   }
 
   if (retryCount && typeof retryCount !== 'number') {
-    return new TypeError('[retryCount] must use number type');
+    return new Error('[retryCount] must use number type');
   }
 };
 
